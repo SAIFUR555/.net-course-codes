3813225010703a2a580124a21142bb4efaa52b6c
Sayed Saifur Rahman <saifurrahmans306@gmail.com>
2026-02-09 18:46:05 +0600

    Initial commit - .NET Lab Codes

---
+++ FirstProject/FirstProject/Controllers/PortfolioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Bio()
+            {
+                return View();
+        }
+        public IActionResult Projects()
+            {
+                return View();
+            }
+
+
+
+        public IActionResult Skills()
+            {
+                return View();
+        }
+
+        
+    }
+}
